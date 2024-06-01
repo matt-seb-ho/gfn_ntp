ccf2fc2cb43a9d12667586799202f9138f6e75a5
Matthew Ho <msho@ucsb.edu>
2024-05-31 17:30:23 -0700

    replaced train.yaml with example; prepared filtering for entry times

--- scripts/filter_theorems.py
+++ scripts/filter_theorems.py
@@ -6,35 +6,40 @@ from tqdm import tqdm
 from typing import Optional
 import math
 import numpy as np
+from pathlib import Path
 from transformers import AutoTokenizer
 # import matplotlib.pyplot as plt
 # plt.style.use("rose-pine-dawn")
 
-def filter_dataset(
-    data_dir: str = "../data/",
-    lean_dojo_split: str = "random",
-    split: str = "train",
+# assumption: this script is in the scripts/ directory
+data_dir = Path(__file__).parents[1] / "data"
+
+def filter_dataset_for_length(
+    data_file_path: Optional[str] = None,
+    data_dir: str = data_dir,
+    benchmark_splits: str = "random", # or "novel_premises"
+    split: str = "train", # or "val", "test"
     tokenizer_id: str = "EleutherAI/llemma_7b",
     min_depth: int = 1,
     max_depth: int = 3,
     token_limit: Optional[int] = None,
-    output_dir: Optional[str] = None,
 ) -> tuple[list[int], list[int]]:
     """
     - creates a data subset with proofs of a maximum depth of `max_depth`.
     - saves the dataset to the provided path
     - gathers statistics about tactic token lengths
     """
-    train_data_path = os.path.join(
+
+    full_data_path = data_file_path or os.path.join(
         data_dir,
         "leandojo_benchmark_4",
-        lean_dojo_split,
+        benchmark_splits,
         f"{split}.json",
     )
     start = time.perf_counter()
-    with open(train_data_path, "r") as f:
-        train_data = json.load(f)
-    print(f"Loaded {len(train_data)} proofs in {time.perf_counter() - start:.2f} seconds")
+    with open(full_data_path, "r") as f:
+        data = json.load(f)
+    print(f"Loaded {len(data)} proofs in {time.perf_counter() - start:.2f} seconds")
     
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_id)
 
@@ -44,7 +49,7 @@ def filter_dataset(
     token_limit = token_limit or math.inf
     
     print("Filtering proofs...")
-    for proof in tqdm(train_data, total=len(train_data)):
+    for proof in tqdm(data, total=len(data)):
         proof_length = len(proof["traced_tactics"])
         proof_lengths.append(proof_length)
         if not (min_depth <= proof_length <= max_depth):
@@ -62,8 +67,8 @@ def filter_dataset(
             filtered.append(proof)
     
     # save the filtered data
-    filename = f"{lean_dojo_split}_{split}_md{max_depth}_tl{token_limit}.json"
-    output_path = os.path.join(output_dir or data_dir, filename)
+    filename = f"{benchmark_splits}_{split}_pl{min_depth}_{max_depth}_tl{token_limit}.json"
+    output_path = os.path.join(data_dir, filename)
     start = time.perf_counter()
     with open(output_path, "w") as f:
         json.dump(filtered, f, indent=2)
@@ -91,43 +96,61 @@ def deciles(data):
         for i in range(0, 100, 10)
     )
 
-def relative_to_repo(path):
-    scripts_dir = os.path.dirname(__file__)
-    return os.path.realpath(os.path.join(scripts_dir, "..", path))
+
+def main():
+    psr = argparse.ArgumentParser()
+
+    # filtering on length (proof steps, and tokens within each step)
+    psr.add_argument("--filter_length", action="store_true", help="filter on proof step count and tokens per step")
+    # - specifying initial data
+    psr.add_argument("--data_dir", default="data/")
+    psr.add_argument("--splits", choices=["random", "novel_premises"], default="random")
+    psr.add_argument("--split", choices=["train", "val", "test"], default="train")
+    psr.add_argument("--tokenizer", type=str, default="EleutherAI/llemma_7b")
+    # - length filter parameters 
+    psr.add_argument("--min_depth", type=int, default=1)
+    psr.add_argument("--max_depth", type=int, default=3)
+    psr.add_argument("--token_limit", type=int, default=None)
+
+    # filtering on entry time (TODO: set threshold default)
+    psr.add_argument("--filter_time", action="store_true", help="filter on (real and estimated) entry time")
+    psr.add_argument("--data_file", type=str)
+    psr.add_argument("--time_file", type=str, help="file containing real entry times")
+    psr.add_argument("--time_threshold", type=float, default=10, help="threshold for entry time (in seconds) to filter on")
+
+    # misc options
+    psr.add_argument("--show_stats", "-ss", action="store_true")
+    args = psr.parse_args()
+
+    if args.filter_length:   
+        proof_lengths, tactic_lengths = filter_dataset_for_length(
+            data_dir=args.data_dir,
+            benchmark_splits=args.splits,
+            split=args.split,
+            tokenizer_id=args.tokenizer,
+            min_depth=args.min_depth,
+            max_depth=args.max_depth,
+            token_limit=args.token_limit,
+        )
+
+        if not args.show_stats:
+            return
+        print(
+            "\n".join([
+                "# Proof Lengths Statistics",
+                summary_statistics(proof_lengths),
+                deciles(proof_lengths),
+                "------------------------------\n"
+                "# Tactic Lengths Statistics\n",
+                "  - note: only theorems with acceptable proof lengths are included",
+                summary_statistics(tactic_lengths),
+                deciles(tactic_lengths),
+            ])
+        )
+    
+    if args.filter_time:
+        # TODO
+        raise NotImplementedError("Filtering on entry time is not yet implemented")
 
 if __name__ == "__main__":
-    psr = argparse.ArgumentParser()
-    psr.add_argument("--data_dir", "-dd", type=str, default="data/")
-    psr.add_argument("--lean_dojo_split", "-ldsp", type=str, default="novel_premises")
-    psr.add_argument("--split", "-sp", type=str, default="train")
-    psr.add_argument("--tokenizer_id", "-tok", type=str, default="EleutherAI/llemma_7b")
-    psr.add_argument("--output_path", "-out", type=str, default="data/")
-    psr.add_argument("--min_depth", "-mind", type=int, default=1)
-    psr.add_argument("--max_depth", "-maxd", type=int, default=3)
-    psr.add_argument("--token_limit", "-maxt", type=int, default=None)
-    args = psr.parse_args()
-    
-    # make data_dir path relative to the repo root
-    
-    proof_lengths, tactic_lengths = filter_dataset(
-        data_dir=relative_to_repo(args.data_dir),
-        lean_dojo_split=args.lean_dojo_split,
-        split=args.split,
-        tokenizer_id=args.tokenizer_id,
-        min_depth=args.min_depth,
-        max_depth=args.max_depth,
-        token_limit=args.token_limit,
-        output_dir=relative_to_repo(args.output_path),
-    )
-
-    print(
-        "\n".join([
-            "# Proof Lengths Statistics",
-            summary_statistics(proof_lengths),
-            deciles(proof_lengths),
-            "------------------------------\n"
-            "# Tactic Lengths Statistics",
-            summary_statistics(tactic_lengths),
-            deciles(tactic_lengths),
-        ])
-    )
+    main()
