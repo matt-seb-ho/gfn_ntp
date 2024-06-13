ced8c1604df321133644e93c116c6ce9f892f959
Matthew Ho <msho@ucsb.edu>
2024-06-12 23:30:38 -0700

    new timing code v2

--- scripts/time_entry.py
+++ scripts/time_entry.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from time import perf_counter
 from enum import Enum
 from typing import Optional
+import signal
 
 from tqdm import tqdm
 
@@ -21,6 +22,7 @@ from lean_dojo import (
     ProofFinished,
     TacticState,
     DojoHardTimeoutError,
+    LeanError,
 )
 print(f"imported from lean_dojo in {perf_counter() - start}s")
 
@@ -28,96 +30,168 @@ print(f"imported from lean_dojo in {perf_counter() - start}s")
 project_root = Path(__file__).parents[1]
 
 
+class HardTimeLimit:
+    def __init__(self, time_limit):
+        self.time_limit = time_limit
+        self.timed_out = False
+    
+    def start(self):
+        signal.signal(signal.SIGALRM, self.alarm_handler)
+        signal.alarm(self.time_limit)
+    
+    def cancel(self):
+        signal.alarm(0) # disable alarm
+        signal.signal(signal.SIGALRM, signal.SIG_DFL) # reset signal handler
+    
+    def alarm_handler(self, signum, frame):
+        raise HitEntryTimeLimit()
+
+
+class HitEntryTimeLimit(Exception):
+    pass
+
+
 class TimeDojoError(Enum):
-    MISMATCH_BEFORE = 1
-    MISMATCH_AFTER = 2
-    SIMULATED_PROOF_UNFINISHED = 3
-    TRACED_PROOF_UNFINISHED = 4
-    BOTH_PROOFS_UNFINISHED = 5
-    OTHER_TACTICS_ERROR = 6
-    OTHER_ENTRY_ERROR = 7
-    HARD_TIMEOUT = 8
+    INCOMPLETE_PROOF = 1
+    LEAN_ERROR = 2
+    EARLY_FINISH = 3
+    ENTRY_TIMEOUT = 4
+    DOJO_TIMEOUT = 5
+    OTHER_ENTRY_ERROR = 6
+    OTHER_TACTIC_ERROR = 7
 
 
 def time_tactics(dojo, initial_state, tacs) -> tuple[list[int], Optional[TimeDojoError]]:
-    # returns
     tactic_times = []
     state = initial_state
-    for tt_ in tacs:
-        if state.pp != tt_["state_before"]:
-            return tactic_times, TimeDojoError.MISMATCH_BEFORE
+    for i, tt_ in enumerate(tacs):
         start = perf_counter()
-        res = dojo.run_tac(state, tt_["tactic"])
+        tactic_result = dojo.run_tac(state, tt_["tactic"])
         tactic_times.append(perf_counter() - start)
-        if isinstance(res, TacticState):
-            if res.pp != tt_["state_after"]:
-                return tactic_times, TimeDojoError.MISMATCH_AFTER
-        else:
-            if not isinstance(res, ProofFinished):
-                if tt_["state_after"] != "no goals":
-                    return tactic_times, TimeDojoError.BOTH_PROOFS_UNFINISHED
-                return tactic_times, TimeDojoError.SIMULATED_PROOF_UNFINISHED
-            if tt_["state_after"] != "no goals":
-                return tactic_times, TimeDojoError.TRACED_PROOF_UNFINISHED
-        state = res
+
+        # v2 update: only interested if (1) tactic failed (2) proof finished early (3) proof never finished
+        if isinstance(tactic_result, LeanError):
+            return tactic_times, TimeDojoError.LEAN_ERROR
+        elif isinstance(tactic_result, ProofFinished) and i < len(tacs) - 1:
+            return tactic_times, TimeDojoError.EARLY_FINISH
+        state = tactic_result
+
+        ### v1 code (used for initial n=500, 4096)
+        #
+        # if state.pp != tt_["state_before"]:
+        #     return tactic_times, TimeDojoError.MISMATCH_BEFORE
+        #
+        # start = perf_counter()
+        # res = dojo.run_tac(state, tt_["tactic"])
+        # tactic_times.append(perf_counter() - start)
+        #
+        # if isinstance(res, TacticState):
+        #     if res.pp != tt_["state_after"]:
+        #         return tactic_times, TimeDojoError.MISMATCH_AFTER
+        # else:
+        #     if not isinstance(res, ProofFinished):
+        #         # if tt_["state_after"] != "no goals":
+        #         #     return tactic_times, TimeDojoError.BOTH_PROOFS_UNFINISHED
+        #         return tactic_times, TimeDojoError.SIMULATED_PROOF_UNFINISHED
+        #     if tt_["state_after"] != "no goals":
+        #         return tactic_times, TimeDojoError.TRACED_PROOF_UNFINISHED
+        #
+        # state = res
+    
+    if not isinstance(state, ProofFinished):
+        return tactic_times, TimeDojoError.INCOMPLETE_PROOF
     return tactic_times, None
 
     
-def time_theorems(thm_dicts, repo, output_dir, timeout, save_every=500, suffix=""):
+def entry_timeout_handler(signum, frame):
+    raise HitEntryTimeLimit
+    
+def time_theorem(
+    thm: Theorem, 
+    time_limit: HardTimeLimit,
+    traced_tactics: list[dict],
+    dojo_timeout: int = 60, 
+) -> tuple[Optional[int], list[int], Optional[TimeDojoError], str]:
+    # returns entry_time, tactic_times, error, error message
+    try:
+        time_limit.start()
+        start = perf_counter()
+        with InitOptimizedDojo(thm, hard_timeout=dojo_timeout) as (dojo, initial_state):
+            entry_time = perf_counter() - start
+            time_limit.cancel()
+            try:
+                tac_times, error = time_tactics(dojo, initial_state, traced_tactics)
+                return entry_time, tac_times, error, ""
+            except Exception as e:
+                return entry_time, [], TimeDojoError.OTHER_TACTIC_ERROR, str(e)
+    except HitEntryTimeLimit:
+        return None, [], TimeDojoError.ENTRY_TIMEOUT, ""
+    except DojoHardTimeoutError:
+        return None, [], TimeDojoError.DOJO_TIMEOUT, ""
+    except Exception as e:
+        return None, [], TimeDojoError.OTHER_ENTRY_ERROR, str(e)
+
+
+def time_theorems(
+    thm_dicts, 
+    repo, 
+    output_dir, 
+    dojo_timeout, 
+    save_every=500, 
+    suffix="",
+    entry_timeout=5,
+):
     errors = defaultdict(list)
     tactics_times = []
     proof_times = []
+    entry_times = {}
     output_dir = Path(output_dir)
+    suffix = f"_{suffix}" if suffix else ""
 
-    suf = f"_{suffix}" if suffix else ""
     # helper routine to write progress to file
-    def save_output(output_file, thm_dicts, errors, tactics_times, proof_times):
-        # skip double counting OTHER_TACTICS_ERROR and OTHER_ENTRY_ERROR
-        total_error_count = sum(
-            len(v) for k, v in errors.items() 
-            if k not in {TimeDojoError.OTHER_TACTICS_ERROR, TimeDojoError.OTHER_ENTRY_ERROR}
-        )
+    def save_output(output_file):
         key_to_str = lambda x: x.name if isinstance(x, Enum) else str(x)
-        errors_with_name_keys = {key_to_str(k): v for k, v in errors.items()}
+        json_friendly_errors = {key_to_str(k): v for k, v in errors.items()}
         res = {
             "theorems": thm_dicts,
-            "errors": errors_with_name_keys,
+            "errors": json_friendly_errors,
             "tactics_times": tactics_times, # all tactic times
             "proof_times": proof_times,     # sum of tactic times for each theorem
-            "total_error_count": total_error_count,
+            "entry_times": entry_times,     # time to enter each theorem
         }
         with open(output_file, "w") as f:
             json.dump(res, f, indent=2)
         return res
 
-    thms_processed = 0
-    for idx, thm_info in tqdm(thm_dicts.items(), total=len(thm_dicts)):
-        try:
-            thm = Theorem(repo, thm_info["file_path"], thm_info["full_name"])
-            start = perf_counter()
-            with InitOptimizedDojo(thm, hard_timeout=timeout) as (dojo, initial_state):
-                thm_info["entry_time"] = perf_counter() - start
-                try:
-                    tac_times, error = time_tactics(dojo, initial_state, thm_info["traced_tactics"])
-                    tactics_times.extend(tac_times)
-                    proof_times.append(sum(tac_times))
-                    if error is not None:
-                        errors[error].append(idx)
-                except Exception as e:
-                    errors[TimeDojoError.OTHER_TACTICS_ERROR].append(idx)
-                    errors[str(e)].append(idx)
-        except DojoHardTimeoutError:
-            thm_info["entry_time"] = timeout
-            errors[TimeDojoError.HARD_TIMEOUT].append(idx)
-        except Exception as e:
-            errors[TimeDojoError.OTHER_ENTRY_ERROR].append(idx)
-            errors[str(e)].append(idx)
+    time_limit = HardTimeLimit(entry_timeout)
+    for i, (thm_idx, thm_info) in enumerate(tqdm(thm_dicts.items())):
+        # construct theorem object
+        thm = Theorem(repo, thm_info["file_path"], thm_info["full_name"])
+        # time theorem entry and try running tactics
+        entry_time, tac_times, error, error_msg = time_theorem(
+            thm,
+            time_limit,
+            thm_info["traced_tactics"],
+            dojo_timeout=dojo_timeout,
+        )
+        # record results
+        entry_times[thm_idx] = entry_time
+        thm_info["entry_time"] = entry_time
+        thm_info["entry_failed"] = (
+            error == TimeDojoError.ENTRY_TIMEOUT
+            or error == TimeDojoError.OTHER_ENTRY_ERROR
+        )
+        tactics_times.extend(tac_times)
+        if error is None or error == TimeDojoError.EARLY_FINISH:
+            proof_times.append(sum(tac_times))
+        elif error is not None:
+            errors[error].append((thm_idx, error_msg))
+
         # routinely save results in case of crash
-        thms_processed += 1
-        if thms_processed % save_every == 0:
-            save_output(output_dir / f"intermediate{thms_processed}{suf}.json", thm_dicts, errors, tactics_times, proof_times)
+        if i != 0 and i % save_every == 0:
+            save_output(output_dir / f"intermediate{i}{suffix}.json")
     
-    return save_output(output_dir / f"final{suf}.json", thm_dicts, errors, tactics_times, proof_times)
+    return save_output(output_dir / f"final{suffix}.json")
 
 
 def main():
@@ -126,14 +200,20 @@ def main():
     psr.add_argument("--input", type=str, default="/mnt/hdd/msho/gfn_ntp/data/random_train_pl1_3_tl30.json", help="theorems to time")
     psr.add_argument("--timeout", type=int, default=60, help="timeout for each theorem")
     psr.add_argument("--suffix", help="suffix to add to output filename")
+    psr.add_argument("--entry_timeout", type=int, default=5, help="timeout for entering theorem")
+    psr.add_argument("--test_run", action="store_true")
     args = psr.parse_args()
 
     with open(args.input) as f:
         data = json.load(f)
     
     # randomly select args.n theorems to time
-    random.seed(42)
-    idxs_to_test = random.sample(range(len(data)), args.n)
+    if args.test_run:
+        # expecting 3965 to be ~1.7s, 8108 to be ~20s (timeout error)
+        idxs_to_test = [3965, 8108]
+    else:
+        random.seed(42)
+        idxs_to_test = random.sample(range(len(data)), args.n)
     theorems = {i: data[i] for i in idxs_to_test}
     
     # setup
