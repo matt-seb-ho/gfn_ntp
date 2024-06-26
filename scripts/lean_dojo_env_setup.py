d98a94d28395860e8af9626e2ca5f7621f317d17
Matthew Ho <msho@ucsb.edu>
2024-06-25 17:35:07 -0700

    configs example update

--- scripts/lean_dojo_env_setup.py
+++ scripts/lean_dojo_env_setup.py
@@ -11,5 +11,8 @@ def prepare_environment_for_lean_dojo(relative_config_path: str = "../configs"):
 
     # lean dojo cache path
     cache_path_key = "CACHE_DIR"
-    if not cache_path_key in os.environ:
+    if (
+        not cache_path_key in os.environ 
+        and config.env_paths.lean_dojo_cache_path is not None
+    ):
         os.environ[cache_path_key] = config.env_paths.lean_dojo_cache_path
