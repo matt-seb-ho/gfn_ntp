1e87f7904346a31eb89d10ec81f9bb6872918750
Matthew Ho <msho@ucsb.edu>
2024-08-12 14:22:26 -0700

    config path update

--- scripts/lean_dojo_env_setup.py
+++ scripts/lean_dojo_env_setup.py
@@ -7,12 +7,12 @@ def prepare_environment_for_lean_dojo(relative_config_path: str = "../configs"):
     if not "GITHUB_ACCESS_TOKEN" in os.environ:
         with hydra.initialize(config_path=relative_config_path, version_base=None):
             config = hydra.compose(config_name="train")
-        load_dotenv(config.env_paths.github_access_token)
+        load_dotenv(config.paths.github_access_token)
 
     # lean dojo cache path
     cache_path_key = "CACHE_DIR"
     if (
         not cache_path_key in os.environ 
-        and config.env_paths.lean_dojo_cache_path is not None
+        and config.paths.lean_dojo_cache_path is not None
     ):
-        os.environ[cache_path_key] = config.env_paths.lean_dojo_cache_path
+        os.environ[cache_path_key] = config.paths.lean_dojo_cache_path
